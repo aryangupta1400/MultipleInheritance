948de9b3cab53278d080ffb926ee7fa6701d9382
Aryan Gupta <Aryan.Gupta@oneadvanced.com>
2022-10-30 18:47:50 +0530

    Coding Standards updated and code modified

--- MultipleInheritance/Program.cs
+++ MultipleInheritance/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+// 5) Create two interface and illustracte how to use multiple inheritance in C#
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,38 @@ namespace MultipleInheritance
     {
         static void Main(string[] args)
         {
+            AdvancedEmployees employees = new AdvancedEmployees();
+            employees.Head();
+            employees.TraineeEngineer();
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IAhmedabadOffice
+    {
+        void Head(); // same method in 2 different interface.
+
+        void TraineeEngineer();
+        
+    }
+
+    interface IBangloreOffice
+    {
+        void Head();
+    }
+
+    class AdvancedEmployees : IAhmedabadOffice, IBangloreOffice
+    {
+        public void Head()
+        {
+            Console.WriteLine("\nKotresh Mutt is Head of Engineering @Banglore Office.");
+            Console.WriteLine("\nAkshay Singhvi is Head of Engineering @Ahmedabad Office.\n");
+        }
+
+        public void TraineeEngineer()
+        {
+            Console.WriteLine("\nAryan Gupta is Trainee Engineering @Ahmedabad Office.");
         }
     }
 }
